44f1809b91526b6ce055a7d2955bd0c5747fdf20
Navodya <it22608772@my.sliit.lk>
2024-05-05 19:41:58 +0530

    add search for select the staff

--- client/src/pages/AssignWorkerToShift.jsx
+++ client/src/pages/AssignWorkerToShift.jsx
@@ -10,6 +10,7 @@ export default function AssignWorkerToShift() {
     const storedData = localStorage.getItem('selectedStaffForShift');
     return storedData ? JSON.parse(storedData) : Object.fromEntries(shifts.map(shift => [shift._id, []]));
   });
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchShiftsAndStaff = async () => {
@@ -58,77 +59,89 @@ export default function AssignWorkerToShift() {
     });
   };
 
+  const filteredStaff = staff.filter(member =>
+    member.username.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    member.id.toLowerCase().includes(searchTerm.toLowerCase())
+    
+  );
+
   return (
     <div className='flex w-3/5 ml-96'>
-      
-    <div className='container mx-auto mt-14'>
-    <h1 className="text-3xl font-bold mb-10 text-center text-slate-500">Assign Workers To Shift</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="p-6 bg-white rounded-md shadow-md">
-          <h2 className="text-lg font-semibold text-gray-800 mb-4">Select Shift</h2>
-          <select value={selectedShift} onChange={handleShiftChange} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
-            <option value="">Select Shift</option>
-            {shifts.map(shift => (
-              <option key={shift._id} value={shift._id}>{shift.shiftname}</option>
-            ))}
-          </select>
-        </div>
-        <div className="p-6 bg-white rounded-md shadow-md">
-          <h2 className="text-lg font-semibold text-gray-800 mb-4">Select Staff</h2>
-          <div className="grid grid-cols-1 gap-2">
-            {staff.map(member => (
-              <div key={member._id} className="flex items-center">
-                <input
-                  type="checkbox"
-                  value={member._id}
-                  checked={selectedStaffForShift[selectedShift]?.includes(member._id)}
-                  onChange={handleStaffChange}
-                  className="mr-2 cursor-pointer"
-                />
-                <label className="cursor-pointer">{member.username} - ID: {member.id} - Type: {member.type}</label>
-              </div>
-            ))}
+      <div className='container mx-auto mt-14'>
+        <h1 className="text-3xl font-bold mb-10 text-center text-slate-500">Assign Workers To Shift</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="p-6 bg-white rounded-md shadow-md">
+            <h2 className="text-lg font-semibold text-gray-800 mb-4">Select Shift</h2>
+            <select value={selectedShift} onChange={handleShiftChange} className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500">
+              <option value="">Select Shift</option>
+              {shifts.map(shift => (
+                <option key={shift._id} value={shift._id}>{shift.shiftname}</option>
+              ))}
+            </select>
           </div>
-        </div>
-      </div>
-
-      <div className="mt-8">
-        {shifts.map(shift => (
-          <div key={shift._id} className="bg-white rounded-md shadow-md p-6 mb-4">
-            <h2 className="text-lg font-semibold text-gray-800 mb-4">{shift.shiftname}</h2>
-            <div className="overflow-x-auto">
-              <table className="w-full table-auto">
-                <thead>
-                  <tr className="bg-gray-200">
-                    <th className="py-2 px-4 border">Name</th>
-                    <th className="py-2 px-4 border">ID</th>
-                    <th className="py-2 px-4 border">Type</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {selectedStaffForShift[shift._id] && selectedStaffForShift[shift._id].length > 0 ? (
-                    selectedStaffForShift[shift._id].map(id => {
-                      const selectedStaffMember = staff.find(member => member._id === id);
-                      return (
-                        <tr key={id}>
-                          <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.username : 'Staff member not found'}</td>
-                          <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.id : '-'}</td>
-                          <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.type : '-'}</td>
-                        </tr>
-                      );
-                    })
-                  ) : (
-                    <tr>
-                      <td className="border px-4 py-2" colSpan="3">No staff selected for this shift.</td>
-                    </tr>
-                  )}
-                </tbody>
-              </table>
+          <div className="p-6 bg-white rounded-md shadow-md">
+            <h2 className="text-lg font-semibold text-gray-800 mb-4">Select Staff</h2>
+            <input
+              type="text"
+              placeholder="Search by Name or ID"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full px-4 py-2 border rounded-md mb-4 focus:outline-none focus:border-blue-500"
+            />
+            <div className="grid grid-cols-1 gap-2">
+              {filteredStaff.map(member => (
+                <div key={member._id} className="flex items-center">
+                  <input
+                    type="checkbox"
+                    value={member._id}
+                    checked={selectedStaffForShift[selectedShift]?.includes(member._id)}
+                    onChange={handleStaffChange}
+                    className="mr-2 cursor-pointer"
+                  />
+                  <label className="cursor-pointer">{member.username} - ID: {member.id} - Type: {member.type}</label>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
+        </div>
+
+        <div className="mt-8">
+          {shifts.map(shift => (
+            <div key={shift._id} className="bg-white rounded-md shadow-md p-6 mb-4">
+              <h2 className="text-lg font-semibold text-gray-800 mb-4">{shift.shiftname}</h2>
+              <div className="overflow-x-auto">
+                <table className="w-full table-auto">
+                  <thead>
+                    <tr className="bg-gray-200">
+                      <th className="py-2 px-4 border">Name</th>
+                      <th className="py-2 px-4 border">ID</th>
+                      <th className="py-2 px-4 border">Type</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {selectedStaffForShift[shift._id] && selectedStaffForShift[shift._id].length > 0 ? (
+                      selectedStaffForShift[shift._id].map(id => {
+                        const selectedStaffMember = staff.find(member => member._id === id);
+                        return (
+                          <tr key={id}>
+                            <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.username : 'Staff member not found'}</td>
+                            <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.id : '-'}</td>
+                            <td className="border px-4 py-2">{selectedStaffMember ? selectedStaffMember.type : '-'}</td>
+                          </tr>
+                        );
+                      })
+                    ) : (
+                      <tr>
+                        <td className="border px-4 py-2" colSpan="3">No staff selected for this shift.</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
