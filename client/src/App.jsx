f2a9b04bace3f57167a22eeb1a8b9042e927a873
Navodya <it22608772@my.sliit.lk>
2024-05-06 16:48:48 +0530

    dinuni

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -69,6 +69,7 @@ export default function App() {
     <BrowserRouter>
   
       <ToastContainer />
+      
       <Routes>
         
       <Route path='/sign-in' element={<SignIn UserType={setUserType} />} />
