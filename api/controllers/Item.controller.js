08c38b21da7abcf9ab195b388fed918b7457cb37
SahanUpendraAcedemic <IT22600370@my.sliit.lk>
2024-05-08 09:48:23 +0530

    Final tweaks

--- api/controllers/Item.controller.js
+++ api/controllers/Item.controller.js
@@ -107,6 +107,43 @@ export const UpdateItems = async(req,res,next) => {
 export const GetItemreport = async(req,res,next) => {
     try{
         const doc = new JSpdf();
+         // Add header border
+        doc.setDrawColor(0); // Set border color to black
+        doc.rect(5, 5, doc.internal.pageSize.getWidth() - 10, 40); // Draw header border with increased height
+
+        // Add header content
+        doc.setFontSize(20);
+        doc.setTextColor(0, 0, 255); // Set color to blue
+        doc.text('Chaminda Stores', doc.internal.pageSize.getWidth() / 2, 20, { align: 'center' });
+        doc.setFontSize(10);
+        doc.setTextColor(0, 0, 0); // Reset color to black
+        doc.setFontSize(10);
+        doc.setTextColor(130,130,130); // Set color to blue
+        doc.text('No 125, Mapatana, Horana', doc.internal.pageSize.getWidth() / 2, 27, { align: 'center' });
+        doc.setFontSize(10);
+        doc.text('TP : 075 - 6175658', doc.internal.pageSize.getWidth() / 2, 34, { align: 'center' });
+        doc.setFontSize(16);
+        // Calculate space needed for date and time text
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString('en-US', { timeZone: 'UTC' });
+        const formattedTime = currentDate.toLocaleTimeString('en-US', { timeZone: 'UTC' });
+        const dateTimeText = 'Date: ' + formattedDate + ' Time: ' + formattedTime;
+        const textWidth = doc.getStringUnitWidth(dateTimeText) * doc.internal.getFontSize() / doc.internal.scaleFactor;
+        const availableWidth = doc.internal.pageSize.getWidth() - 20; // Subtracting 20 to provide padding
+        const xPos = 104;
+        const yPos = 40; // Adjust as needed
+
+        // Add current date and time
+        doc.setFontSize(10);
+        doc.setTextColor(0, 0, 0); // Set color to black
+        doc.text(dateTimeText, xPos, yPos, { align: 'center' }); // Adjust the position as needed
+
+        // Add document border
+        doc.rect(5, 5, doc.internal.pageSize.getWidth() - 10, doc.internal.pageSize.getHeight() - 10); // Draw document border
+
+        // Add title with underline
+        doc.setFontSize(16);
+        doc.setDrawColor(0); // Set140,140 underline color to black
         res.setHeader('Content-Type','application/pdf');
         res.setHeader('Content-Disposition','attachment; filename=Itemreport.pdf');
         doc.fontSize(25).text('Item Report',100,100);
